b2ee6d66268ba639e0df88e5c74d74dfce64e58f
jason-zhang-github <zhang.jason.2021@gmail.com>
2021-09-24 11:04:29 -0400

    created ImageAdapter class and ImageObject data class

---
+++ app/src/main/java/com/example/imageactivityapp/ImageAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.imageactivityapp
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+
+
+class ImageAdapter (val_context : Context, _imageObjects: Array<ImageObject>): BaseAdapter()
+{
+    val imageObjects = _imageObjects
+    val
+}
---
+++ app/src/main/java/com/example/imageactivityapp/ImageObject.kt
@@ -0,0 +1,3 @@
+package com.example.imageactivityapp
+
+data class ImageObject (val description: String, val resourceID: Int)
