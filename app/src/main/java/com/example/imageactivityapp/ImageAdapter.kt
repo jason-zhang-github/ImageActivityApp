ff2611e9d32f1ffe34c05b97eb46e4a524d11288
jason-zhang-github <zhang.jason.2021@gmail.com>
2021-09-30 00:00:23 -0400

    Finished rudimentary holder class and oncreateviewholder

--- app/src/main/java/com/example/imageactivityapp/ImageAdapter.kt
+++ app/src/main/java/com/example/imageactivityapp/ImageAdapter.kt
@@ -13,11 +13,13 @@ import androidx.constraintlayout.widget.ConstraintLayout
 
 /**/
 
-class ImageAdapter (val _imageList : ArrayList<ImageObject>, val click : (ImageObject) -> Unit) : RecyclerView.Adapter<ImageAdapter.ImageHolder>() {
+class ImageAdapter (val _imageList : ArrayList<ImageObject>) : RecyclerView.Adapter<ImageAdapter.ImageHolder>() {
 
     // public ImageAdapter.MyViewHolder onCreateViewHolder
 
-    class ImageHolder(_imageView: View, onClick: (ImageObject) -> Unit) :
+    var images = _imageList
+
+    class ImageHolder(_imageView: View, /*onClick: (ImageObject) -> Unit*/) :
         RecyclerView.ViewHolder(_imageView)
     {
         var imageVal: ImageView
@@ -29,75 +31,20 @@ class ImageAdapter (val _imageList : ArrayList<ImageObject>, val click : (ImageO
                 current?.let { onClick(it) } */
             }
     }
-}
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageAdapter.ImageHolder {
 
-        /*val inflater : View = LayoutInflater.from(parent.context).inflate(R.layout.recycler_layout)
-        return ImageHolder(inflater)*/
+        val inflater : View = LayoutInflater.from(parent.context).inflate(R.layout.image_recycler, parent, false)
+        return ImageHolder(inflater)
     }
 
     override fun onBindViewHolder(holder: ImageAdapter.ImageHolder, position: Int) {
-        TODO("Not yet implemented")
     }
 
     override fun getItemCount(): Int {
         return _imageList.size
     }
-
-    // val images = _images // image dataset
-   // val inflater = LayoutInflater.from(_context)
-    /*
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
-    {
-        val imageView = ImageView(parent.context)
-                return ImageViewHolder(imageView)
-            //.inflate(R.layout.recycler_layout, parent, false)
-
-        return ViewHolder(imageView)
-    }
-
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int)
-    {
-        // viewHolder.imageView = _images[position]
-    }
-
-    override fun getItemCount() = _images.size
-
-    fun getItem(position : Int) : Any
-    {
-        return images.get(position)
-    }
-
-    override fun getItemId(position: Int) : Long
-    {
-        return position.toLong()
-    }
-
-
-    fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View
-    {
-        var layout : View
-
-        if (convertView is ConstraintLayout)
-        {
-            layout = convertView
-        }
-
-        else
-        {
-            layout = inflater.inflate(R.layout.activity_main, null)
-        }
-
-        val description = layout.findViewById<TextView>(R.id.textView)
-        val imageView = layout.findViewById<ImageView>(R.id.imageView)
-
-        imageView.setImageResource(images[position].resourceID)
-        description.text = images[position].description
-
-        return layout
-    }
-    */
 
 
 }
